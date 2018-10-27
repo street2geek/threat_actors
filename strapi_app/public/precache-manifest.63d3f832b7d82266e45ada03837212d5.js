ab74518560d5ccea6a425eaaf17e824493bbfd3c
Wafiq Baugh <screenlantern@gmail.com>
2018-10-27 04:24:23 +0100

    built frontend

---
+++ strapi_app/public/precache-manifest.63d3f832b7d82266e45ada03837212d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0e031793f61305043ff0",
+    "url": "/js/chunk-vendors.7cfa584c.js"
+  },
+  {
+    "revision": "40ab31657e282e2b27ca",
+    "url": "/js/app.a859b061.js"
+  },
+  {
+    "revision": "a553b841cdaba320a3f8394ac65758d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e031793f61305043ff0",
+    "url": "/css/chunk-vendors.3b0fae3a.css"
+  },
+  {
+    "revision": "40ab31657e282e2b27ca",
+    "url": "/css/app.7aa87266.css"
+  }
+];
